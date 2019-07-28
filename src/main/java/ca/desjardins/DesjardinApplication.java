fad7365ae91c6ebc65aab98a68acf946a0ac7958
Lenia lazo Hdez <lenialazohdez@monrestosaintmichel.org>
2019-07-27 22:26:30 -0400

    1

---
+++ src/main/java/ca/desjardins/DesjardinApplication.java
@@ -0,0 +1,13 @@
+package ca.desjardins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesjardinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesjardinApplication.class, args);
+	}
+
+}
